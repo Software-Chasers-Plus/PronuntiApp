ca47e6b1749f2373e0d1ad8b9baabd6ac89666e8
EmanuelePiemontese <e.piemontese@studenti.uniba.it>
2024-01-17 14:11:44 +0100

    Modificata la SplashActivity per gestire anche il logopedista

--- app/src/main/java/it/uniba/dib/sms232419/pronuntiapp/SplashActivity.java
+++ app/src/main/java/it/uniba/dib/sms232419/pronuntiapp/SplashActivity.java
@@ -44,8 +44,7 @@ public class SplashActivity extends AppCompatActivity {
         public void handleMessage(Message msg) {
             switch (msg.what) {
                 case GO_AHEAD_WHAT:
-                    long elapsedTime = SystemClock.uptimeMillis() -
-                            mStartTime;
+                    long elapsedTime = SystemClock.uptimeMillis() - mStartTime;
                     if ((elapsedTime >= MIN_WAIT_INTERVAL && !mIsDone) && fecthCompletato) {
                         mIsDone = true;
                         startMainActivity();
@@ -57,91 +56,97 @@ public class SplashActivity extends AppCompatActivity {
                         startMainActivity();
                     }
                     break;
-
             }
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.splash_screen);
-            View decorView = getWindow().getDecorView();
-            int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-            decorView.setSystemUiVisibility(uiOptions);
-            fetchDataFromdataBase();
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
+        decorView.setSystemUiVisibility(uiOptions);
+        fetchDataFromdataBase();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
         mStartTime = SystemClock.uptimeMillis();
-        final Message goAheadMessage =
-                mHandler.obtainMessage(GO_AHEAD_WHAT);
-        mHandler.sendMessageAtTime(goAheadMessage, mStartTime +
-                MAX_WAIT_INTERVAL);
+        final Message goAheadMessage = mHandler.obtainMessage(GO_AHEAD_WHAT);
+        mHandler.sendMessageAtTime(goAheadMessage, mStartTime + MAX_WAIT_INTERVAL);
     }
-
 
     private void fetchDataFromdataBase() {
-            FirebaseAuth auth = FirebaseAuth.getInstance();
-            FirebaseFirestore db = FirebaseFirestore.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-            //verifico se l'utente è già loggato
-            if (auth.getCurrentUser() != null) {
-                //lancio la query per verificare se l'utente è un genitore
-                db.collection("genitori")
-                        .document(auth.getCurrentUser().getUid())
-                        .get()
-                        .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                            @Override
-                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                if (task.isSuccessful()) {
-                                    DocumentSnapshot document = task.getResult();
-                                    if (document.exists()) {
-                                        //se è un genitore recupero recuperaro i suoi figli
-
-                                        //lancio la query per recuperare i figli del genitore
-                                        db.collection("figli")
-                                                .whereEqualTo("genitore", auth.getCurrentUser().getUid())
-                                                .get()
-                                                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                                                    @Override
-                                                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                                                        if (task.isSuccessful()) {
-                                                            //se i figli vengono trovati li passo all'activity principale e la faccio partire
-                                                            Log.d("Accessoactivity", "Figli trovati con query");
-                                                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                                                Map<String, Object> nuovoFiglio = document.getData();
-                                                                figli.add(new Figlio(nuovoFiglio.get("nome").toString(), nuovoFiglio.get("cognome").toString(),
-                                                                        nuovoFiglio.get("codiceFiscale").toString(), nuovoFiglio.get("logopedista").toString(),
-                                                                        FirebaseAuth.getInstance().getCurrentUser().getUid(), nuovoFiglio.get("dataNascita").toString()));
-                                                            }
-                                                            loggato = true;
-                                                            fecthCompletato = true;
-                                                            final Message goAheadMessage =
-                                                                    mHandler.obtainMessage(FECTH_TERMINATO);
-                                                            mHandler.sendMessage(goAheadMessage);
+        // Verifico se l'utente è già loggato
+        if (auth.getCurrentUser() != null) {
+            // Lancio la query per verificare se l'utente è un genitore
+            db.collection("genitori")
+                    .document(auth.getCurrentUser().getUid())
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                if (document.exists()) {
+                                    // Se è un genitore, recupero i suoi figli
+                                    db.collection("figli")
+                                            .whereEqualTo("genitore", auth.getCurrentUser().getUid())
+                                            .get()
+                                            .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                                                    if (task.isSuccessful()) {
+                                                        for (QueryDocumentSnapshot document : task.getResult()) {
+                                                            Map<String, Object> nuovoFiglio = document.getData();
+                                                            figli.add(new Figlio(
+                                                                    nuovoFiglio.get("nome").toString(),
+                                                                    nuovoFiglio.get("cognome").toString(),
+                                                                    nuovoFiglio.get("codiceFiscale").toString(),
+                                                                    nuovoFiglio.get("logopedista").toString(),
+                                                                    FirebaseAuth.getInstance().getCurrentUser().getUid(),
+                                                                    nuovoFiglio.get("dataNascita").toString()
+                                                            ));
                                                         }
+                                                        loggato = true;
+                                                        fecthCompletato = true;
+                                                        final Message goAheadMessage = mHandler.obtainMessage(FECTH_TERMINATO);
+                                                        mHandler.sendMessage(goAheadMessage);
                                                     }
-                                                });
-                                    } else {
-                                        //TODO: gestire il caso in cui l'utente è un logopedista
-                                    }
+                                                }
+                                            });
                                 } else {
-                                    //TODO: gestire il caso in cui la query per verificare che è un genitore non va a buon fine
+                                    // L'utente è un logopedista
+                                    handleLogopedista();
                                 }
+                            } else {
+                                // TODO: Gestire il caso in cui la query per verificare che è un genitore non va a buon fine
                             }
-                        });
-            }else{
-                loggato = false;
-                fecthCompletato = true;
-            }
+                        }
+                    });
+        } else {
+            loggato = false;
+            fecthCompletato = true;
+        }
     }
 
+    private void handleLogopedista() {
+        // L'utente è un logopedista
+        // Avvia l'activity principale per il logopedista
+        Intent intent = new Intent(this, MainActivityLogopedista.class);
+        startActivity(intent);
+        finish();
+    }
+
+
     private void startMainActivity() {
-        if(loggato == true){
-            //creo il bundle da passare all'activity principale
+        if (loggato) {
+            // Creo il bundle da passare all'activity principale
             Bundle bundle = new Bundle();
             bundle.putParcelableArrayList("figli", (ArrayList<? extends Parcelable>) figli);
 
@@ -149,10 +154,10 @@ public class SplashActivity extends AppCompatActivity {
             Intent intent = new Intent(this, MainActivity.class);
             intent.putExtras(bundle);
 
-            //faccio partire l'activity principale
+            // Faccio partire l'activity principale
             startActivity(intent);
             finish();
-        }else{
+        } else {
             startActivity(new Intent(this, AccessoActivity.class));
             finish();
         }
