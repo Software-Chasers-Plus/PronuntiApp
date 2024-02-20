08fcfdfa05841874a30cb7cd9c0f5983d80ced4a
Angelo <angelosciarra14@gmail.com>
2024-02-20 09:30:15 +0100

    retrive degli esercizi dal database in GiocoActivity

--- app/src/main/java/it/uniba/dib/sms232419/pronuntiapp/Gioco/GiocoActivity.java
+++ app/src/main/java/it/uniba/dib/sms232419/pronuntiapp/Gioco/GiocoActivity.java
@@ -3,6 +3,9 @@ package it.uniba.dib.sms232419.pronuntiapp.Gioco;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.KeyEvent;
 
@@ -21,15 +24,34 @@ import java.util.Map;
 
 import it.uniba.dib.sms232419.pronuntiapp.AccessoActivity;
 import it.uniba.dib.sms232419.pronuntiapp.R;
+import it.uniba.dib.sms232419.pronuntiapp.model.Esercizio;
+import it.uniba.dib.sms232419.pronuntiapp.model.EsercizioTipologia1;
+import it.uniba.dib.sms232419.pronuntiapp.model.EsercizioTipologia2;
+import it.uniba.dib.sms232419.pronuntiapp.model.EsercizioTipologia3;
 import it.uniba.dib.sms232419.pronuntiapp.model.Genitore;
 import it.uniba.dib.sms232419.pronuntiapp.model.Scheda;
 
 public class GiocoActivity extends AppCompatActivity {
 
+    private static final int RETRIVE_ESERCIZI_COMPLETATO = 1;
+    private static int NUMERO_ESERCIZI;
+    private static int NUMERO_ESERCIZI_SCARICATI;
     public MediaPlayer mediaPlayer;
     public Integer sfondoSelezionato = 0,personaggioSelezionato = 0;
     private Scheda scheda;
+    private ArrayList<Esercizio> esercizi = new ArrayList<>();
     FirebaseFirestore db;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+            case RETRIVE_ESERCIZI_COMPLETATO:
+                mostraFragmentAvvioGioco();
+                break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,13 +71,7 @@ public class GiocoActivity extends AppCompatActivity {
             Log.d("GiocoActivity", "Scheda: " + scheda.getNome() + " caricata");
         }
 
-
-        Bundle bundle = new Bundle();
-        bundle.putParcelable("scheda", scheda);
-        getSupportFragmentManager().beginTransaction()
-                .setReorderingAllowed(true)
-                .add(R.id.avvio_gioco_fragment, AvvioGiocoFragment.class, bundle)
-                .commit();
+        retriveEserciziScheda();
     }
 
     @Override
@@ -84,6 +100,78 @@ public class GiocoActivity extends AppCompatActivity {
         if (mediaPlayer != null) {
             mediaPlayer.release();
             mediaPlayer = null;
+        }
+    }
+
+    private void mostraFragmentAvvioGioco(){
+        Bundle bundle = new Bundle();
+        bundle.putParcelable("scheda", scheda);
+        Log.d("GiocoActivity", "Fragment AvvioGiocoFragment caricato");
+        getSupportFragmentManager().beginTransaction()
+                .setReorderingAllowed(true)
+                .add(R.id.avvio_gioco_fragment, AvvioGiocoFragment.class, bundle)
+                .commit();
+    }
+
+    private void retriveEserciziScheda(){
+        db = FirebaseFirestore.getInstance();
+        NUMERO_ESERCIZI = scheda.getNumeroEsercizi();
+        NUMERO_ESERCIZI_SCARICATI = 0;
+
+        for(int i=0; i<NUMERO_ESERCIZI; i++){
+            db.collection("esercizi")
+                    .document(scheda.getEsercizi().get(i).get(0))
+                    .get()
+                    .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                        @Override
+                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                            if (task.isSuccessful()) {
+                                DocumentSnapshot document = task.getResult();
+                                Map<String, Object> esercizio = document.getData();
+                                switch (Integer.valueOf(esercizio.get("tipologia").toString())){
+                                    case 1:
+                                        esercizi.add(new EsercizioTipologia1(esercizio.get("tipologia").toString(),
+                                                esercizio.get("nome").toString(),
+                                                esercizio.get("logopedista").toString(),
+                                                esercizio.get("tipologia").toString(),
+                                                esercizio.get("immagine").toString(),
+                                                esercizio.get("descrizioneImmagine").toString(),
+                                                esercizio.get("audio1").toString(),
+                                                esercizio.get("audio2").toString(),
+                                                esercizio.get("audio3").toString()));
+                                        NUMERO_ESERCIZI_SCARICATI++;
+                                        break;
+                                    case 2:
+                                        esercizi.add(new EsercizioTipologia2(esercizio.get("tipologia").toString(),
+                                                esercizio.get("nome").toString(),
+                                                esercizio.get("logopedista").toString(),
+                                                esercizio.get("tipologia").toString(),
+                                                esercizio.get("audio").toString(),
+                                                esercizio.get("trascrizione_audio").toString()));
+                                        NUMERO_ESERCIZI_SCARICATI++;
+                                        break;
+                                    case 3:
+                                        esercizi.add(new EsercizioTipologia3(esercizio.get("tipologia").toString(),
+                                                esercizio.get("nome").toString(),
+                                                esercizio.get("logopedista").toString(),
+                                                esercizio.get("tipologia").toString(),
+                                                esercizio.get("audio").toString(),
+                                                esercizio.get("immagine1").toString(),
+                                                esercizio.get("immagine2").toString(),
+                                                Long.valueOf(esercizio.get("immagine_corretta").toString())));
+                                        NUMERO_ESERCIZI_SCARICATI++;
+                                        break;
+                                }
+                                if(NUMERO_ESERCIZI_SCARICATI == NUMERO_ESERCIZI){
+                                    mHandler.sendEmptyMessage(RETRIVE_ESERCIZI_COMPLETATO);
+                                }
+                                Log.d("GiocoActivity", "Esercizio " + esercizi.get(NUMERO_ESERCIZI_SCARICATI-1).getNome() + " scaricato tipologia: " + esercizi.get(NUMERO_ESERCIZI_SCARICATI-1).getTipologia());
+
+                            } else {
+                                NUMERO_ESERCIZI_SCARICATI++;
+                            }
+                        }
+                    });
         }
     }
 
